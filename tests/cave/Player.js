6507e5210c116fc642570664c749522bb11acd12
devjgames <109635665+devjgames@users.noreply.github.com>
2026-07-03 12:16:41 -0700

    cave update
    cave update

--- tests/cave/Player.js
+++ tests/cave/Player.js
@@ -16,7 +16,7 @@ function create(me) {
 function init(me) {
 	if(!me.scene().isInDesign()) {
 		me.scene().target.set(me.node().position.x, 0, me.node().position.z);
-		me.scene().target.add(0, 700, 900, me.scene().eye);
+		me.scene().target.add(0, 400, 400, me.scene().eye);
 		me.scene().up.set(0, 1, 0);
 	}
 }
@@ -44,9 +44,10 @@ function update(me) {
 		x = Math.max(me.scene().root.bounds.min.x + 256, x);
 		x = Math.min(me.scene().root.bounds.max.x - 256, x);
 		z = Math.max(me.scene().root.bounds.min.z + 128, z);
-		z = Math.min(me.scene().root.bounds.max.z - 256 - 64, z);
+		z = Math.min(me.scene().root.bounds.max.z - 256, z);
 		me.scene().target.set(x, 0, z);
-		me.scene().target.add(0, 700, 900, me.scene().eye);
+		me.scene().target.add(0, 400, 400, me.scene().eye);
+		me.node().getChild(2).rotate(1, 90 * game.elapsedTime());
 	}
 }
 
